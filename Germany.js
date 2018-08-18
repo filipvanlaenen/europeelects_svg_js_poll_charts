bcb2ea9ab2188320e929e25472d4a3049517a3a3
Filip van Laenen <f.a.vanlaenen@ieee.org>
2018-08-18 21:09:13 +0200

    Added Germany and support for countries with 6 and 7 parties

--- Germany.js
+++ Germany.js
@@ -10,30 +10,28 @@
           //
           // Rearrange the parties according to size, and fill in the percentage:
           //
-          fillParty(svg, no_of_decimals, "1", "S", 23.6);
-          fillParty(svg, no_of_decimals, "2", "M", 19.7);
-          fillParty(svg, no_of_decimals, "3", "SD", 19.6);
-          fillParty(svg, no_of_decimals, "4", "C", 11.1);
-          fillParty(svg, no_of_decimals, "5", "V", 8.8);
-          fillParty(svg, no_of_decimals, "6", "MP", 6.5);
-          fillParty(svg, no_of_decimals, "7", "L", 4.8);
-          fillParty(svg, no_of_decimals, "8", "KD", 3.6);
-          fillParty(svg, no_of_decimals, "9", "Fi", 1.0);
+          fillParty(svg, no_of_decimals, "1", "CDU/CSU", 31.0);
+          fillParty(svg, no_of_decimals, "2", "SPD", 17.0);
+          fillParty(svg, no_of_decimals, "3", "AfD", 15.0);
+          fillParty(svg, no_of_decimals, "4", "GRÜNE", 14.0);
+          fillParty(svg, no_of_decimals, "5", "LINKE", 10.0);
+          fillParty(svg, no_of_decimals, "6", "FDP", 8.0);
+          fillParty(svg, no_of_decimals, "7", "CDU", 25.0);
+          fillParty(svg, no_of_decimals, "8", "CSU", 6.0);
           //
           // *******************************************************************
           //
         }
         function fillParty(svg, no_of_decimals, index, name, result) {
           var ep_groups = {};
-          ep_groups["C"] = "ALDE";
-          ep_groups["Fi"] = "S&D";
-          ep_groups["L"] = "ALDE";
-          ep_groups["KD"] = "EPP";
-          ep_groups["M"] = "EPP";
-          ep_groups["MP"] = "Greens/EFA";
-          ep_groups["S"] = "S&D";
-          ep_groups["SD"] = "ECR";
-          ep_groups["V"] = "GUE/NGL";
+          ep_groups["AfD"] = "EFDD";
+          ep_groups["CDU"] = "EPP";
+          ep_groups["CDU/CSU"] = "EPP";
+          ep_groups["CSU"] = "EPP";
+          ep_groups["FDP"] = "ALDE";
+          ep_groups["GRÜNE"] = "Greens/EFA";
+          ep_groups["LINKE"] = "GUE/NGL";
+          ep_groups["SPD"] = "S&D";
           var colors = {};
           colors["ALDE"] = "#FFD700";
           colors["ECR"] = "#0000FF";
@@ -43,15 +41,14 @@
           colors["GUE/NGL"] = "#990000";
           colors["S&D"] = "#FF0000";
           var last_results = {};
-          last_results["C"] = 6.1;
-          last_results["Fi"] = 3.1;
-          last_results["L"] = 5.4;
-          last_results["KD"] = 4.6;
-          last_results["M"] = 23.3;
-          last_results["MP"] = 6.9;
-          last_results["S"] = 31.0;
-          last_results["SD"] = 12.9;
-          last_results["V"] = 5.7;
+          last_results["AfD"] = 12.6;
+          last_results["CDU"] = 26.8;
+          last_results["CDU/CSU"] = 33.0;
+          last_results["CSU"] = 6.2;
+          last_results["FDP"] = 10.7;
+          last_results["GRÜNE"] = 8.9;
+          last_results["LINKE"] = 9.2;
+          last_results["SPD"] = 20.5;
           svg.getElementById("party-name-" + index).firstChild.nodeValue = name;
           svg.getElementById("ep-group-name-" + index).firstChild.nodeValue = ep_groups[name];
           svg.getElementById("party-bar-" + index).setAttribute("height", result * 20);
