ad59dc67f1c536bab5d59aa90194a33a985dbd49
Filip van Laenen <f.a.vanlaenen@ieee.org>
2018-11-17 17:20:54 +0100

    Automatic sorting and automatic scaling

--- Austria.js
+++ Austria.js
@@ -1,56 +1,39 @@
-        function fillChart(svg) {
+        function getResults() {
+          var results = {};
           //
           // *******************************************************************
           //
-          // Set the number of decimals behing the point, either 0 or 1:
-          //   E.g. setting it to 1 will round to 12.3%
-          //        setting it to 0 will round to 12%
+          // Fill in the percentage, or set to a negative number if the party
+          // was absent in the poll:
           //
-          var no_of_decimals = 0;
-          //
-          // Rearrange the parties according to size, and fill in the percentage:
-          //
-          fillParty(svg, no_of_decimals, "1", "ÖVP", 33);
-          fillParty(svg, no_of_decimals, "2", "SPÖ", 26);
-          fillParty(svg, no_of_decimals, "3", "FPÖ", 23);
-          fillParty(svg, no_of_decimals, "4", "NEOS", 8);
-          fillParty(svg, no_of_decimals, "5", "PILZ", 3);
-          fillParty(svg, no_of_decimals, "6", "GRÜNE", 3);
+          results["ÖVP"] = 33;
+          results["SPÖ"] = 26;
+          results["FPÖ"] = 23;
+          results["NEOS"] = 8;
+          results["PILZ"] = 3;
+          results["GRÜNE"] = 3;
           //
           // *******************************************************************
           //
+          return results;
         }
-        function fillParty(svg, no_of_decimals, index, name, result) {
-          var ep_groups = {};
-          ep_groups["FPÖ"] = "ENF";
-          ep_groups["GRÜNE"] = "Greens/EFA";
-          ep_groups["NEOS"] = "ALDE";
-          ep_groups["ÖVP"] = "EPP";
-          ep_groups["PILZ"] = "*";
-          ep_groups["SPÖ"] = "S&D";
+        function getLastResults() {
           var last_results = {};
-          last_results["FPÖ"] = 26.0;
-          last_results["GRÜNE"] = 3.8;
-          last_results["NEOS"] = 5.3;
           last_results["ÖVP"] = 31.4;
-          last_results["PILZ"] = 4.4;
           last_results["SPÖ"] = 26.9;
-          var colors = getEpGroupColors();
-          var max_value = 32.0;
-          svg.getElementById("party-name-" + index).firstChild.nodeValue = name;
-          svg.getElementById("ep-group-name-" + index).firstChild.nodeValue = ep_groups[name];
-          svg.getElementById("party-bar-" + index).setAttribute("height", barHeight(result, max_value));
-          svg.getElementById("party-bar-" + index).setAttribute("y", 830 - barHeight(result, max_value));
-          svg.getElementById("party-bar-" + index).setAttribute("fill", colors[ep_groups[name]]);
-          svg.getElementById("party-bar-last-" + index).setAttribute("height", barHeight(last_results[name], max_value));
-          svg.getElementById("party-bar-last-" + index).setAttribute("y", 830 - barHeight(last_results[name], max_value));
-          svg.getElementById("party-bar-last-" + index).setAttribute("fill", colors[ep_groups[name]]);
-          svg.getElementById("percentage-" + index).firstChild.nodeValue = result.toFixed(no_of_decimals) + "%";
-          svg.getElementById("percentage-" + index).setAttribute("y", 815 - barHeight(result, max_value));
-          svg.getElementById("percentage-last-" + index).firstChild.nodeValue = last_results[name].toFixed(no_of_decimals) + "%";
-          svg.getElementById("percentage-last-" + index).setAttribute("y", 815 - barHeight(last_results[name], max_value));
-          if (last_results[name] < result) {
-            var last_result_x = parseInt(svg.getElementById("percentage-last-" + index).getAttribute("x"));
-            svg.getElementById("percentage-last-" + index).setAttribute("x", last_result_x + 20);
-          }
+          last_results["FPÖ"] = 26.0;
+          last_results["NEOS"] = 5.3;
+          last_results["PILZ"] = 4.4;
+          last_results["GRÜNE"] = 3.8;
+          return last_results;
+        }
+        function getEpGroupMapping() {
+          var ep_group_mapping = {};
+          ep_group_mapping["FPÖ"] = "ENF";
+          ep_group_mapping["GRÜNE"] = "Greens/EFA";
+          ep_group_mapping["NEOS"] = "ALDE";
+          ep_group_mapping["ÖVP"] = "EPP";
+          ep_group_mapping["PILZ"] = "*";
+          ep_group_mapping["SPÖ"] = "S&D";
+          return ep_group_mapping;
         }
--- common.js
+++ common.js
@@ -1,8 +1,76 @@
 				function setData(e) {
 					var svg = e.target.ownerDocument;
 					setTitle(svg);
-          fillChart(svg);
+          var results = getResults();
+          var last_results = getLastResults();
+          var ep_group_mapping = getEpGroupMapping();
+          //
+          // *******************************************************************
+          //
+          // Set the number of decimals behing the point, either 0 or 1:
+          //   E.g. setting it to 1 will round to 12.3%
+          //        setting it to 0 will round to 12%
+          //
+          var no_of_decimals = 0;
+          //
+          // *******************************************************************
+          //
+          drawChart(svg, results, last_results, ep_group_mapping,
+                    no_of_decimals);
 				}
+        function drawChart(svg, results, last_results, ep_group_mapping,
+                           no_of_decimals) {
+          var colors = getEpGroupColors();
+          var all_parties = Object.keys(results);
+          var parties = [];
+          for (var i = 0; i < all_parties.length; i++) {
+            var party = all_parties[i];
+            if (results[party] >= 0) {
+              parties.push(party);
+            }
+          }
+          parties.sort(function (a, b) {
+            return results[b] - results[a];
+          });
+          var no_of_parties = parties.length;
+          var result_values = [];
+          var max_value = 0.0;
+          for (var i = 0; i < no_of_parties; i++) {
+            var party = parties[i];
+            if (results[party] > max_value) {
+              max_value = results[party];
+            }
+            if (last_results[party] > max_value) {
+              max_value = last_results[party];
+            }
+          }
+          for (var i = 0; i < no_of_parties; i++) {
+            var party = parties[i];
+            var ep_group = ep_group_mapping[party];
+            drawBar(svg, no_of_decimals, max_value, (i + 1).toFixed(0), party,
+                    results[party], last_results[party], ep_group,
+                    colors[ep_group]);
+          }
+        }
+        function drawBar(svg, no_of_decimals, max_value, index, name, result,
+                         last_result, ep_group, color) {
+          svg.getElementById("party-name-" + index).firstChild.nodeValue = name;
+          svg.getElementById("ep-group-name-" + index).firstChild.nodeValue = ep_group;
+          svg.getElementById("party-bar-" + index).setAttribute("height", barHeight(result, max_value));
+          svg.getElementById("party-bar-" + index).setAttribute("y", 830 - barHeight(result, max_value));
+          svg.getElementById("party-bar-" + index).setAttribute("fill", color);
+          svg.getElementById("party-bar-last-" + index).setAttribute("height", barHeight(last_result, max_value));
+          svg.getElementById("party-bar-last-" + index).setAttribute("y", 830 - barHeight(last_result, max_value));
+          svg.getElementById("party-bar-last-" + index).setAttribute("fill", color);
+          svg.getElementById("percentage-" + index).firstChild.nodeValue = result.toFixed(no_of_decimals) + "%";
+          svg.getElementById("percentage-" + index).setAttribute("y", 815 - barHeight(result, max_value));
+          svg.getElementById("percentage-last-" + index).firstChild.nodeValue = last_result.toFixed(no_of_decimals) + "%";
+          svg.getElementById("percentage-last-" + index).setAttribute("y", 815 - barHeight(last_result, max_value));
+          if (last_result < result) {
+            var last_result_x = parseInt(svg.getElementById("percentage-last-" + index).getAttribute("x"));
+            svg.getElementById("percentage-last-" + index).setAttribute("x", last_result_x + 20);
+          }
+        }
         function barHeight(value, max_value) {
           return value * 20 * 25 / max_value;
         }
